c23dce0615dec542c540f739e40eace3f542d43e
Jurrit van der Ploeg <jurrit.van.der.ploeg@vanderlande.com>
2022-03-07 09:51:01 +0100

    0018 Add SCSS to Storybook / Icon stories

---
+++ src/elements/icon/icon-element.stories.ts
@@ -0,0 +1,30 @@
+import { html } from 'lit';
+
+import '../../../dist';
+
+import { Story } from '../../stories';
+
+import './icon-element.stories.scss';
+import { Icon } from './icon';
+
+export default {
+  title: 'Component/Icons',
+  component: 'button-element'
+};
+
+interface ArgTypes {
+  name: Icon;
+}
+
+const Template: Story<ArgTypes> = ({ name }: ArgTypes) => {
+  return html`
+    <aotw-icon class="story-icon" .name=${name}>
+      Button
+    </aotw-icon>
+  `;
+};
+
+export const IconLogo = Template.bind({});
+IconLogo.args = {
+  name: Icon.LOGO
+};
