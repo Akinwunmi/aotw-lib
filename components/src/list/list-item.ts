d97ab371df7c37b9354cddb15499c00e586adc8d
Jurrit van der Ploeg <jurritvdploeg@hotmail.com>
2022-10-22 23:54:09 +0200

    feat/container: Add container / Update list item

--- components/src/list/list-item.ts
+++ components/src/list/list-item.ts
@@ -1,4 +1,4 @@
-import { LitElement, TemplateResult, html, unsafeCSS } from 'lit';
+import { TemplateResult, html, unsafeCSS, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 import styleListItem from './list-item.scss';
@@ -6,12 +6,8 @@ import styleListItem from './list-item.scss';
 const AOTW_LIST_ITEM = 'aotw-list-item';
 
 @customElement(AOTW_LIST_ITEM)
-export class ListItemElement extends LitElement {
-  public static override styles = unsafeCSS(styleListItem);
-
-  protected override firstUpdated(): void {
-    this.requestUpdate();
-  }
+export class AotwListItem extends LitElement {
+  public static override styles = [unsafeCSS(styleListItem)];
 
   protected override render(): TemplateResult {
     return html`
@@ -22,6 +18,6 @@ export class ListItemElement extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    AOTW_LIST_ITEM: ListItemElement
+    AOTW_LIST_ITEM: AotwListItem
   }
 }
