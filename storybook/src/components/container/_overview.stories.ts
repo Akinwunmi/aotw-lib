71bb430be71543f761b4d4fd746d824bd278ea94
Jurrit van der Ploeg <jurritvdploeg@hotmail.com>
2023-03-30 17:59:15 +0200

    feat(scrollable-container): move content next to scrollbar

--- storybook/src/components/container/_overview.stories.ts
+++ storybook/src/components/container/_overview.stories.ts
@@ -8,6 +8,8 @@ export default {
   title: 'Components/Container'
 } as Meta;
 
+const content = `What I have to do, I have to catch everybody if they start to go over the cliff \u2014 I mean if they're running and they don't look where they're going I have to come out from somewhere and catch them. That's all I do all day. I'd just be the catcher in the rye and all.`;
+
 export const Overview = () => html`
   <h2>Container</h2>
   <div class="overview">
@@ -16,13 +18,13 @@ export const Overview = () => html`
       <div class="variant">
         <header>Default</header>
         <div class="content">
-          <aotw-container></aotw-container>
+          <aotw-container>Content</aotw-container>
         </div>
       </div>
       <div class="variant">
         <header>Elevated</header>
         <div class="content">
-          <aotw-container elevated></aotw-container>
+          <aotw-container elevated>Content</aotw-container>
         </div>
       </div>
     </section>
@@ -31,21 +33,19 @@ export const Overview = () => html`
       <div class="variant">
         <header>Default</header>
         <div class="content">
-          <aotw-container scrollable></aotw-container>
+          <aotw-container scrollable>${content}</aotw-container>
         </div>
       </div>
       <div class="variant">
         <header>Elevated</header>
         <div class="content">
-          <aotw-container elevated scrollable>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium illo harum cum suscipit expedita obcaecati enim deleniti, vitae sit ratione. Sunt ratione porro ea dolore ab error iusto. Veritatis quam hic voluptate sapiente obcaecati ea provident. Quidem voluptas hic cupiditate vel nostrum, ipsam laboriosam eos doloribus fugiat, ipsa totam perspiciatis facilis corporis obcaecati nisi? A nemo architecto beatae repudiandae!
-          </aotw-container>
+          <aotw-container elevated scrollable>${content}</aotw-container>
         </div>
       </div>
       <div class="variant">
         <header>Disabled</header>
         <div class="content">
-          <aotw-container elevated scrollable disabled></aotw-container>
+          <aotw-container elevated scrollable disabled>${content}</aotw-container>
         </div>
       </div>
     </section>
